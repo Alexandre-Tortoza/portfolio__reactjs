ff933246adf84d198b8bdf56c438d704abd0bc50
Alexandre Marques <a.marquestortoza@gmail.com>
2023-02-18 22:49:15 -0300

    teste

--- src/components/Works.js
+++ src/components/Works.js
@@ -3,7 +3,6 @@ import loginpage from "./../images/loginPage.jpg"
 import styles from "./Works.module.css"
 import WorkGroup from "./WorkGroup";
 import Works__VetCare from "../pages/Works__VetCare";
-import Works__LoginPage from './../pages/works__LoginPage'
 import {
     BrowserRouter as Router,
     Routes,
@@ -23,7 +22,7 @@ function Works() {
     <Router>
         <Routes>
             <Route path="/" element={<WorkGroup/>} />
-            <Route path="/works/loginpage" element={<Works__LoginPage/>} />
+            {/* <Route path="/works/loginpage" element={<Works__LoginPage/>} /> */}
             <Route path="/works/vetcare" element={<Works__VetCare/>} />
 
         </Routes>
