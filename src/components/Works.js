25ae12191767210c50499a730535e360417e6920
Alexandre Marques <a.marquestortoza@gmail.com>
2023-02-18 22:53:09 -0300

    .

--- src/components/Works.js
+++ src/components/Works.js
@@ -3,6 +3,7 @@ import loginpage from "./../images/loginPage.jpg"
 import styles from "./Works.module.css"
 import WorkGroup from "./WorkGroup";
 import Works__VetCare from "../pages/Works__VetCare";
+import Works__LoginPage from './../pages/works__LoginPage'
 import {
     BrowserRouter as Router,
     Routes,
@@ -22,7 +23,7 @@ function Works() {
     <Router>
         <Routes>
             <Route path="/" element={<WorkGroup/>} />
-            {/* <Route path="/works/loginpage" element={<Works__LoginPage/>} /> */}
+            <Route path="/works/loginpage" element={<Works__LoginPage/>} />
             <Route path="/works/vetcare" element={<Works__VetCare/>} />
 
         </Routes>
